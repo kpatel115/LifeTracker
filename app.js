240d93e9b212af72132277401e6ed854b99896ad
kpatel115 <kp9338448@gmail.com>
2023-04-26 14:06:37 -0500

    connected MongoDB - created card js/pug

--- app.js
+++ app.js
@@ -3,11 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cardRouter = require('./routes/card');
+
 
 var app = express();
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://kpatel115:f17ios89@cluster0.uemyhfp.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/card', cardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/cardMongoDBRepo.js
+++ src/cardMongoDBRepo.js
@@ -4,13 +4,12 @@ const databaseName = process.env.DB_NAME;
 const databaseUser = process.env.DB_USER;
 const databasePass = process.env.DB_PASS;
 
-//const url = `mongodb+srv://${databaseUser}:${databasePass}@${databaseName}.uemyhfp.mongodb.net/?retryWrites=true&w=majority`;
-const url = 'mongodb+srv://kpatel115:<password>@cluster0.uemyhfp.mongodb.net/?retryWrites=true&w=majority';
+
 const client = new MongoClient(url);
 
 var date = new Date(); 
 var newdate = date.toGMTString();
-
+/*
 async function run() {
   await client.connect();
   return 'Connected to the MongoDB server...';
@@ -19,7 +18,7 @@ async function run() {
 run()
   .then(console.log)
   .catch(console.error);
-
+*/
 const repo = {
   findAll: async () => {
     let cards = []; // list of cards
