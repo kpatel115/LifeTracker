e3550cecf24b9b8525a63686793fdb199cdaa6a2
kpatel115 <kp9338448@gmail.com>
2023-05-01 11:52:35 -0500

    working on creating users and google authentication/passport

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var dotenv = require('dotenv');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var passport = require('passport');
 var session = require('express-session')
-var dotenv = require('dotenv');
+
 var bcrypt = require('bcrypt');
 
+// Connecting routes and urls
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cardRouter = require('./routes/cards');
 var loginRouter = require('./routes/users');
 var registerRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
+
+dotenv.config({ path: './config/config.env'})
+
+// Passport Config
+require('./config/passport')(passport)
 
 var app = express();
 
@@ -24,16 +30,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enabling routes and URLS
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cards', cardRouter);
-
 app.use('/login', loginRouter)
 app.use('/register', registerRouter);
 app.use('/auth', authRouter);
